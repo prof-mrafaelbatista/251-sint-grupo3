ca3f3d41d9db71809038641957911a8298da3fcf
hrasam <hrasamgomes@gmail.com>
2025-05-30 09:29:39 -0300

    muito pouco tempo para o desenvolvimento do projeto, me rendi a ter que usar ia para acelerar a criação do projeto, embora não seja um indicativo de real aprendizado.
    simboraaa que falta muitooo.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-app.run()
+@app.route('/')
+def index():
+return render_template('index.html', page_title='pagina inicial')
+
+@app.route('/outra-pagina')
+def outra_pagina():
+return render_template('outra_pagina.html', page_title='outra pagina')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
