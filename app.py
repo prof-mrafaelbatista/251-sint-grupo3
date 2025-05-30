800f0b1cc9a3a5103e4174b2655c505438816822
hrasam <hrasamgomes@gmail.com>
2025-05-30 10:03:43 -0300

    corringindo erro na sintaxe, também criei o arquivo do css

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-return render_template('index.html', page_title='pagina inicial')
+    return render_template('index.html', page_title='pagina inicial')
 
 @app.route('/outra-pagina')
 def outra_pagina():
-return render_template('outra_pagina.html', page_title='outra pagina')
+    return render_template('outra_pagina.html', page_title='outra pagina')
 
 
 if __name__ == '__main__':
